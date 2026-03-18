76fb55955e43524738c9f5723ed63b012ccb4920
mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-03-18 15:00:39 +0100

    Coperate internet banking app created

---
+++ src/main/java/org/app/corporateinternetbanking/CorporateInternetBankingApplication.java
@@ -0,0 +1,13 @@
+package org.app.corporateinternetbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateInternetBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateInternetBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/app/corporateinternetbanking/CorporateInternetBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.corporateinternetbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateInternetBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
