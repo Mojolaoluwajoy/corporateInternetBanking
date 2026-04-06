0a10b46c2715cfab420b939f96d9efca4e32b30d
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-04-06 18:50:49 +0100

    Switch from mysql to postgreSQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
-INSERT  IGNORE INTO currency(name,code,symbol,status) VALUES
+INSERT INTO currency(name,code,symbol,status) VALUES
 ('Nigerian Naira', 'NGN', '#','INACTIVE'),
 ('US Dollar','USD','$','INACTIVE'),
 ('Euro','EUR','E','INACTIVE'),
 ('Canadian Dollar','CAD','C$','INACTIVE'),
-('Australian Dollar','AUD','A$','INACTIVE');
+('Australian Dollar','AUD','A$','INACTIVE')
+ON CONFLICT (code) DO NOTHING;
 
